6732e1197f4ae5d70bd77970595bb0d706236695
Mukhammed <muhammedlazgiev404@gmail.com>
2024-09-11 11:55:04 +0500

    first commit (save)

---
+++ src/main/java/com/example/salam123/Salam123Application.java
@@ -0,0 +1,14 @@
+package com.example.salam123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Salam123Application {
+
+    public static void main(String[] args) {
+        int n = 123;
+        SpringApplication.run(Salam123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salam123/Salam123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salam123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Salam123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
